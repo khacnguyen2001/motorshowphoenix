1f3e146fab6bbc3a32baf4d1bdb3cf8ee096c52a
coolnlite <mtauto.marketing@gmail.com>
2022-10-20 12:43:14 +0700

    ok

--- modules/add_data_customers.php
+++ modules/add_data_customers.php
@@ -12,22 +12,21 @@ if($_POST['fullname'] && $_POST['phone'] && $_POST['models'] && $_POST['number_c
     $code = 'MTG-'.substr($number_car, -5);
     
     $url = 'uploads/qr/'.$code.'-'.time().'.png';
-    $path = $base_url.$url;
-	$name =  $path;
-	QRcode::png($code,$name,'H',5,5);
+    $path = '../'.$url;
+	QRcode::png($code,$path,'H',5,5);
     $qr_code = $url;
-    
+    $status = 0;
     $sql ="SELECT `phone` FROM `customers` WHERE `phone` = '$phone'";
     $result = mysqli_query($conn,$sql);
     if(mysqli_num_rows($result) == 0){
-        $sql = "INSERT INTO `customers` (`code`,`fullname`,`phone`,`models`,`number_car`,`qr_code`) VALUES('$code','$fullname','$models','$number_car','$qr_code')";
+        $sql = "INSERT INTO `customers` (`code`,`fullname`,`phone`,`models`,`number_car`,`qr_code`,`status`) VALUES('$code','$fullname','$models','$number_car','$qr_code','$status')";
         $result = mysqli_query($conn,$sql);
         echo json_encode(array(
             'status' => 1
             ));
         exit();
     }else{
-        $sql = "UPDATE `customers` SET `code` = '$code', `fullname` = '$fullname', `phone` = '$phone',`models` = '$models', `number_car` = '$number_car', `qr_code` = '$qr_code' WHERE `phone` = $phone";
+        $sql = "UPDATE `customers` SET `code` = '$code', `fullname` = '$fullname', `phone` = '$phone',`models` = '$models', `number_car` = '$number_car', `qr_code` = '$qr_code',`status` =`$status` WHERE `phone` = $phone";
         $result = mysqli_query($conn,$sql);
         echo json_encode(array(
             'status' => 1
